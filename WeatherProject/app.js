61abafd7da7ff67925da454cf34e706f04c8134a
williamszk <wllmszk@gmail.com>
2022-02-06 23:45:40 -0300

    webdev API checkpoint

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,28 +1,50 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
-app.get("/", function (req, res) {
+app.use(bodyParser.urlencoded({extended: true}));
 
-	const urlHolder = "https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo&appid=f428395adc097581d6a93ff833e8eb32&units=metric"
-	https.get(urlHolder, function(response) {
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+	const query = req.body.cityName
+	const apiKey = "55be9c12e582c70a186b66653977b6ee"
+	const units = "metric"
+	const urlHolder = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+	https.get(urlHolder, function (response) {
 		console.log(response.statusCode);
 
-		response.on("data", function(data) {
+		response.on("data", function (data) {
 			const weatherData = JSON.parse(data);
 			// console.log(weatherData);
 
 			const temperature = weatherData.main.temp;
+
 			const weatherDescription = weatherData.weather[0].description;
-			
-			res.write("<p>The weather is curretnly " + weatherDescription + "</p>");
-			res.write("<h1>The temperature in Sao Paulo is "+temperature+ " degrees Celcius.</h1>");
-			res.write('<img src="http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png" alt="weather_image">');
+
+			res.write("<p>The weather is currently " + weatherDescription + "</p>");
+			res.write(
+			"<h1>The temperature in " + query + " is " +
+				temperature +
+				" degrees Celcius.</h1>"
+			);
+			res.write(
+			'<img src="http://openweathermap.org/img/wn/' +
+				weatherData.weather[0].icon +
+				'@2x.png" alt="weather_image">'
+			);
 			res.send();
 		});
 	});
 });
+
+
 
 app.listen(3000, function () {
 	console.log("Server is running on port 3000.");
