abb22e1bd412abb3dcf7427d3a729471a1d8d0c4
williamszk <wllmszk@gmail.com>
2022-02-13 16:31:46 -0300

    webdev checkpoint

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,10 +1,23 @@
 const bodyParser = require("body-parser");
-const request = require("request");
 const express = require("express");
+const request = require("request");
+
+
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const query = req.body;
+    console.log(query);
+});
 
 
 
@@ -13,5 +26,6 @@ const app = express();
 
 
 app.listen(3000, function() {
-    console.log("Server started on port 3000.")
+    console.log("Server started on port 3000.");
 });
+
