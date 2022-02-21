d008192961c35aeecb5b6afacf051027502cb983
williamszk <wllmszk@gmail.com>
2022-02-21 20:11:15 -0300

    challenge blog

---
+++ ejs-challenge/app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+const { lowerCase } = require("lodash");
+
+const posts = [];
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  
+  // const postsTruncated = posts.map(post => {
+  //   const truncatedPost = {...post};
+  //   truncatedPost.textContentPineApple = truncatedPost.textContentPineApple.substring(0, 100)+"...";
+  //   return truncatedPost;
+  // });
+
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const query = req.body;
+  const post = {
+    textTitlePineApple: query.textTitlePineApple,
+    textContentPineApple: query.textContentPineApple,
+  };
+
+  posts.push(post);
+  res.redirect("/");
+});
+
+// this is: dynamic url, routing parameters
+app.get("/posts/:postName", (req, res) => {
+  // Another post -> another post
+  // another-post -> another post
+  const postName = lodash.lowerCase(req.params.postName);
+  const doesPostNameExist = posts.some((post) => {
+    return lodash.lowerCase(post.textTitlePineApple) === postName;
+  });
+  if (doesPostNameExist) {
+    const postsToShow = posts.filter(
+      (post) => lodash.lowerCase(post.textTitlePineApple) === postName
+    );
+    res.render("post", {
+      postTitle: postsToShow[0].textTitlePineApple,
+      postContent: postsToShow[0].textContentPineApple,
+    });
+  } else {
+    console.log("Post does not exist.");
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
+
+// use "rs" restart nodemon
+// it will reset all objects i memory
