e2e5e90de4d39e958e69cd216e789fef3a61eb09
williamszk <wllmszk@gmail.com>
2022-03-26 13:36:43 -0300

    blog challenge connecting with mongodb

--- todolist-v3/app.js
+++ todolist-v3/app.js
@@ -13,6 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Here I think we are connecting with MongoDB Atlas
 const secret = JSON.parse(fs.readFileSync("secret.json"));
 mongoose.connect(
   "mongodb+srv://admin-william:" +
