08f6269d526bcd79d4ccd65dde07d393cf3f1dad
williamszk <wllmszk@gmail.com>
2022-03-31 02:22:11 +0000

    experiments javascript and game

--- experimental_game_01/app.js
+++ experimental_game_01/app.js
@@ -1,1 +1,19 @@
 // this is an experimental game
+
+const express = require("express");
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "index.html");
+});
+
+app.listen(3000, () => {
+	console.log("Server started at port 3000.")
+});
+
+// some ideas:
+// https://www.youtube.com/watch?v=iPsDJMVrz5Q&t=46s&ab_channel=VrTech
+// The settlers 3?
+// 
---
+++ experimental_game_01/public/index.js
@@ -0,0 +1,37 @@
+// alert("Hello there!");
+
+document.querySelector("h1").innerHTML = "Good bye!";
+
+// document.getElementsByTagName()
+// document.getElementsByClassName()
+// document.getElementById()
+
+// let anelement01 = document.getElementsByTagName("h1");
+// console.log(anelement01)
+// console.log(typeof(anelement01));
+
+// let anelement01 = document.querySelector("h1");
+// console.log(anelement01);
+// console.log(typeof(anelement01));
+
+// document.querySelector("#title");
+// document.querySelector("li a");
+// document.querySelector("li.item"); // comnination of selectors, those are characteristics that are in the same level in the html element
+// document.querySelector("#list a"); // this is called a hierarchical selector given the same between the two selectors
+// document.querySelectorAll("#list a"); // this will return an array with all the elements that match the query
+
+// document.querySelector("button").classList; // this gives a list of the classes in the element
+// document.querySelector("button").classList.add("mynewclass") // how to add a new class
+// document.querySelector("button").classList.remove("mynewclass")
+// document.querySelector("button").classList.toggle("mynewclass")
+
+
+
+
+
+
+
+
+
+
+
