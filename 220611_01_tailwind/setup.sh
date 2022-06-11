b76ccba265afa3da914c11f9dd2978e708850d53
williamszk <wllmszk@gmail.com>
2022-06-11 20:57:50 -0300

    try to tinker with tailwind

---
+++ 220611_01_tailwind/setup.sh
@@ -0,0 +1,18 @@
+
+
+# https://tailwindcss.com/docs/installation
+
+# https://www.youtube.com/watch?v=pfaSUYaSgRo&ab_channel=Fireship
+
+npx create-react-app headwind
+
+npm install -D tailwindcss
+
+# what -D does?
+
+# this will overwrite the default post css configuration
+npm install @craco/craco --legacy-peer-deps
+
+
+touch craco.config.js
+
