73e8f298f371dbe7bc363422bb00e1b96a0167fd
williamszk <wllmszk@gmail.com>
2023-07-25 01:37:09 +0000

    add application for testing with cypress

---
+++ udemy-cypress/230724/notes.sh
@@ -0,0 +1,12 @@
+
+# https://github.com/bondar-artem/ngx-cypress-test
+
+# we need to clone this application we'll use it as the app in which we'll 
+# be going to be making tests
+git clone https://github.com/bondar-artem/ngx-cypress-test.git
+
+cd ngx-cypress-test
+npm i --force
+npm start
+
+
---
+++ udemy-cypress/notes.sh
@@ -0,0 +1,11 @@
+
+# Notes based on the udemy course:
+# https://www.udemy.com/course/cypress-web-automation-testing-from-zero-to-hero/learn/lecture/15863430#overview
+
+node -v
+
+
+
+
+
+
